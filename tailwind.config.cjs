b01885943c844729294c434a766facc5ec5bc814
qilingzon <qilingzon@qq.com>
2026-01-01 14:16:44 +0800

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+    darkMode: "class", // allows toggling dark mode manually
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+            },
+            transitionDuration: {
+                '300': '300ms',
+                '600': '600ms',
+            }, // Custom transition duration
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+}
